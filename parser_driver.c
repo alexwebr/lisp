98f2817bdfc2563d16624cb46e5b2307c6d67ca1
Alex Weber <alexwebr@gmail.com>
2014-01-09 22:49:38 -0600

    Added tree-freeing routine to make Valgrind happy
    Will refactor all of the tree stuff out. It's getting a life of
    its own, like the stack stuff.

--- parser_driver.c
+++ parser_driver.c
@@ -30,6 +30,21 @@ void print_tree(unsigned int depth, tree_t *p)
   }
 }
 
+void free_tree(tree_t *p)
+{
+  if (p->sibling != NULL) {
+    free_tree(p->sibling);
+    p->sibling = NULL;
+  }
+
+  if (p->child != NULL) {
+    free_tree(p->child);
+    p->child = NULL;
+  }
+
+  free(p);
+}
+
 int main()
 {
   char buf[100000];
@@ -41,4 +56,5 @@ int main()
 
   tree_t *p = create_parse_tree(buf, i);
   print_tree(0, p);
+  free_tree(p);
 }
